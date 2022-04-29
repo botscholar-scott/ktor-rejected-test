c348b0e1fcbff157ac321fa1d9008bf8131a7fc9
srankin <45577128+botscholar-scott@users.noreply.github.com>
2022-04-29 08:59:19 -0700

    updated to use thread for the client and a supervisor job and added a couple asserts;

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ktor-rejected-test
-demo of ktor client connecting to ktor server to illustrate connection error
+Test illustrating ktor client connecting to ktor server to illustrate connection error on slower computers
+
+Steps to reproduce:
+1. mvn test
--- src/test/kotlin/KtorTest.kt
+++ src/test/kotlin/KtorTest.kt
@@ -13,20 +13,25 @@ import io.ktor.response.*
 import io.ktor.server.engine.*
 import kotlinx.coroutines.*
 import org.junit.jupiter.api.Test
-import java.time.LocalDateTime
 import java.time.LocalDateTime.*
+
+private const val MESSAGE = "Hello, world!"
 
 fun getServer(): NettyApplicationEngine {
     return embeddedServer(Netty, 8080) {
         routing {
             get("/") {
-                call.respondText("Hello, world!", ContentType.Text.Html)
+                call.respondText(MESSAGE, ContentType.Text.Html)
             }
         }
     }
 }
 
 class KtorClientTest {
+    private val clientThreads = newSingleThreadContext("client")
+    private val syncJob = SupervisorJob()
+    private lateinit var actualStatusCode: HttpStatusCode
+    private lateinit var actualMessage: String
 
     @OptIn(ExperimentalCoroutinesApi::class, DelicateCoroutinesApi::class)
     @Test
@@ -41,30 +46,37 @@ class KtorClientTest {
                 server.start(wait = true)
                 System.err.println("[${now()}] after server.start() in launch block")
             }
-            System.err.println("[${now()}] Before server launch delay()")
-            delay(60000)
-            System.err.println("[${now()}] After server launch delay()")
+            System.err.println("[${now()}] Before call to getData")
+            getData()
+            System.err.println("[${now()}] After call to getData")
         } finally {
             System.err.println("[${now()}] Calling server.stop() in finally block...")
             server.stop(1000, 20000)
         }
+        assertEquals(HttpStatusCode.OK, actualStatusCode)
+        assertEquals(MESSAGE, actualMessage)
     }
 
     private fun onStarted(application: Application) {
         System.err.println("[${now()}] ******** onStarted ********")
-        runBlocking {
+        syncJob.complete()
+    }
+    private suspend fun getData() = withContext(clientThreads) {
+        launch(clientThreads) {
+            System.err.println("[${now()}] Waiting for start")
+            syncJob.join()
+            System.err.println("[${now()}] Started")
             val client = HttpClient(CIO)
-            delay(10000)
+            delay(150)
             System.err.println("[${now()}] Before client.get()")
             try {
                 val response: HttpResponse = client.get("http://localhost:8080/")
-                System.err.println("[${now()}] After client.get() ${response.status}")
-                val actual: String = response.receive()
-                System.err.println("actual? ${actual}")
+                System.err.println("[${now()}] After client.get()")
+                actualStatusCode = response.status
+                actualMessage = response.receive()
             } catch (e: Throwable) {
                 System.err.println(e.printStackTrace())
-            }
-            finally {
+            } finally {
                 client.close()
             }
         }
